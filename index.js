0835c9fa3fe2eb43ca5f3c986ee0b9c9b531fa41
gsladeira02 <gsladeira02@gmail.com>
2026-04-29 11:47:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 
 app.use('/api', routes)
